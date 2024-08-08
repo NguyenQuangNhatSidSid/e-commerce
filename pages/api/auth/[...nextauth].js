fbd994c2e52b5602dd378221790237258c5c115b
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-08-08 15:58:31 +0700

    add auth, nav bar

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
-
 import GoogleProvider from 'next-auth/providers/google'
-
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from "@/lib/db"
 
 export default NextAuth({
   providers: [
@@ -23,5 +23,6 @@ export default NextAuth({
     //   server: process.env.MAIL_SERVER,
     //   from: 'NextAuth.js <no-reply@example.com>'
     // }),
-  ]
+  ],
+  adapter: MongoDBAdapter(client)
 })
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/NAV";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session) {
+  const { data: session } = useSession();
+  if (!session) {
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
         <div className="text-center w-full">
-        <button onClick={()=> signIn('google')} className="bg-white text-blue-900 p-2 px-4 rounded-lg">Login with Google</button>
-        </div> 
-       </div>
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white text-blue-900 p-2 px-4 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
     );
   }
-  return(
-    <div>logged in{session.user.email}</div>
-  )
+  return (
+    <div className="bg-blue-900 min-h-screeen flex ">
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+        logged in{session.user.email}
+      </div>
+    </div>
+  );
 }
