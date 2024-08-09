1efa52eb472bcaa5b5618d4e29aa9decda9ab721
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-08-09 15:59:56 +0700

    get information of user by session and show in dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,25 @@
-import Nav from "@/components/NAV";
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white text-blue-900 p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello,
+          <b> {session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+          <img
+            src={session?.user?.image}
+            alt={session?.user?.name || "user image"}
+            className="w-6 h-6 rounded-full overflow-hidden"
+          />
+          <span className="py-1 px-2">{session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-  return (
-    <div className="bg-blue-900 min-h-screeen flex ">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-        logged in{session.user.email}
-      </div>
-    </div>
+    </Layout>
   );
 }
---
+++ pages/products.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import Link from "next/link";
+
+export default function Products() {
+  return (
+    <Layout>
+      <Link className="bg-blue-300 rounded-md py-1 px-2" href={"/products/new"}>
+        add
+      </Link>
+    </Layout>
+  );
+}
