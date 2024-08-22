06503cc9536997d022c10874fbeee02b90fe6fc2
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-08-22 17:10:41 +0700

    add edit category

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -6,8 +6,14 @@ export default async function handle(req, res) {
   await mongooseConnect();
   try {
     if (method === "POST") {
-      const { name } = req.body;
-      const categoryDoc = await Category.create({ name });
+      const { name, parentCategory } = req.body;
+      const categoryDoc = await Category.create({
+        name,
+        parent: parentCategory,
+      });
+      res.status(201).json(categoryDoc);
+    } else if (method === "GET") {
+      const categoryDoc = await Category.find().populate("parent");
       res.status(201).json(categoryDoc);
     } else {
       res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
--- pages/categories.js
+++ pages/categories.js
@@ -1,24 +1,44 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Categories() {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  async function fetchCategories() {
+    await axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
 
   async function saveCategory(ev) {
     ev.preventDefault();
     try {
-      await axios.post("/api/categories", { name });
+      await axios.post("/api/categories", { name, parentCategory });
       setName("");
+      fetchCategories();
     } catch (error) {
       console.error("Error processing request:", error);
     }
+  }
+  async function editCategory(category) {
+    setEditedCategory(category);
   }
 
   return (
     <Layout>
       <h1>Categories</h1>
-      <label>New Category name</label>
+      <label>
+        {editedCategory
+          ? `Edit Category ${editedCategory.name}`
+          : "New Category name"}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-1">
         <input
           type="text"
@@ -27,16 +47,49 @@ export default function Categories() {
           value={name}
           onChange={(ev) => setName(ev.target.value)}
         />
+        <select
+          className="mb-0"
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option>No parent category</option>
+          {categories.length > 0 &&
+            categories.map((cate) => (
+              <option key={cate._id} value={cate._id}>
+                {cate.name}
+              </option>
+            ))}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
       </form>
-      <table>
+      <table className="basic mt-4">
         <thead>
           <tr>
             <td>Category name</td>
+            <td>Parent category</td>
+            <td></td>
           </tr>
         </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((cate) => (
+              <tr key={cate._id}>
+                <td>{cate.name}</td>
+                <td>{cate?.parent?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(cate)}
+                    className=" btn-primary mr-1"
+                  >
+                    Edit
+                  </button>
+                  <button className=" btn-primary">Delete</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </Layout>
   );
