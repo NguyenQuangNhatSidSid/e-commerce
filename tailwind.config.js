9609c81a9c4b7f1e7256e9ba1d51f625de05380b
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-09-05 16:46:58 +0700

    add tail wind color, navbar md

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5542F6",
+        hightlight: "#EAE8FB",
+        bgGray: "#fbfafd",
+      },
+    },
   },
   plugins: [],
-}
+};
