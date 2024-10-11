7cf9daec49eec2062a9041049f195056a3d3119f
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-10-11 17:18:15 +0700

    add orders api ,page , and model

---
+++ pages/orders.js
@@ -0,0 +1,48 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function OrdersPage() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => setOrders(response.data));
+  }, []);
+
+  return (
+    <Layout>
+      <h1>Orders</h1>
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Recipient</th>
+            <th>Recipient</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr key={order.length}>
+                <td>{order.createAt}</td>
+                <td>
+                  {order.name} {order.email} <br />
+                  {order.city} {order.postalCode} {order.country} <br />
+                  {order.stressAddress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data?.name} x {l.quantity}
+                      {JSON.stringify(l)}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
